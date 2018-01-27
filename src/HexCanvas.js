b1346caa072742505376abe9a6eb64a6c5f04198
Gavin Nishizawa <ghn6069@rit.edu>
2018-01-27 13:18:04 -0500

    Add hex canvas

---
+++ src/HexCanvas.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class HexCanvas extends Component {
+
+    componentDidMount() {
+        let canvas = ReactDOM.findDOMNode(this.refs.hexCanvas);
+        let context = canvas.getContext('2d');
+
+        context.fillStyle = 'rgb(100,0,0)';
+        context.fillRect(100,100,120,80);
+    }
+
+    render() {
+        return (
+            <div className="hex-canvas">
+                <canvas id="hex-canvas" ref="hexCanvas" width="1280" height="800">
+
+
+                </canvas>
+            </div>
+        );
+    }
+}
+
+export default HexCanvas;
