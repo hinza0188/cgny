f81e9815379d761222be7eefb19786c1e836577d
Yongki Jay An <hinza0188@gmail.com>
2018-01-27 16:34:31 -0500

    hex grid made out of triangle has been established

--- src/HexCanvas.js
+++ src/HexCanvas.js
@@ -48,9 +48,24 @@ class HexCanvas extends Component {
         context.fillStyle = bg.color;
         context.fillRect(bg.x, bg.y, bg.width, bg.height);
 
-        Hexagon.drawTriangleU(context, 0, 100, 50, "blue");
-        Hexagon.drawTriangleD(context, 100, 100, 50, "red");
-        Hexagon.drawTriangleU(context, 100,100, 50, "purple");
+        let size = 50;
+
+
+        for(var i = 0; i<=15; i++){
+            for(var j=0; j<=15; j++){
+                if (j%2 === 1) {
+                    Hexagon.drawTriangleU(context, (size*2*i), size*j*Math.sqrt(3), size, "blue");
+                    Hexagon.drawTriangleD(context, (size*2*i+1), size*j*Math.sqrt(3), size, "red");
+                } else {
+                    Hexagon.drawTriangleU(context, (size*2*i) - (size), size*j*Math.sqrt(3), size, "blue");
+                    Hexagon.drawTriangleD(context, (size*2*i+1) - (size), size*j*Math.sqrt(3), size, "red");
+                }
+            }
+        }
+        //Hexagon.drawTriangleU(context, 0, 100, 50, "blue");
+        //Hexagon.drawTriangleD(context, 100, 100, 50, "red");
+
+        //Hexagon.drawTriangleU(context, 100,100, 50, "purple");
     }
 
     render() {
