72fa716b025673a3dd656b1c7e0667899157004a
Yongki Jay An <hinza0188@gmail.com>
2018-01-27 19:01:52 -0500

    revised app title

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">CGNY: Cellular Automaton</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <HexCanvas />
       </div>
